486bcedc6548f346ca90cfb433376e20d974bf9b
rly <rly@lbl.gov>
2025-09-03 14:57:40 -0700

    Comment out mapping class until ready

--- src/pynwb/ndx_multisubjects/ndx_multisubjects_nwb_file_io.py
+++ src/pynwb/ndx_multisubjects/ndx_multisubjects_nwb_file_io.py
@@ -1,18 +1,18 @@
-from pynwb import register_map
-from pynwb.io.file import NWBFileMap
-from . import NdxMultiSubjectsNWBFile
+# from pynwb import register_map
+# from pynwb.io.file import NWBFileMap
+# from . import NdxMultiSubjectsNWBFile
 
 
-# NOTE: When the NWBEP001 is merged into the core NWB schema and software, this class will be merged
+# NOTE: When this extension is merged into the core NWB schema and software, this class will be merged
 # with the core NWBFileMap class.
-@register_map(NdxMultiSubjectsNWBFile)
-class NdxMultiSubjectsNWBFileMap(NWBFileMap):
+# @register_map(NdxMultiSubjectsNWBFile)
+# class NdxMultiSubjectsNWBFileMap(NWBFileMap):
 
-    def __init__(self, spec):
-        super().__init__(spec)
+#     def __init__(self, spec):
+#         super().__init__(spec)
 
-        # Map the "events" attribute on the NdxEventsNWBFile class to the EventsTable class
-        # general_spec = self.spec.get_group("general")
-        # self.unmap(general_spec)
-        # self.unmap(general_spec.get_group("SubjectsTable"))
-        # self.map_spec("subjects_table", general_spec.get_group("SubjectsTable"))
+# Map the "subjects_table" attribute on the NdxMultiSubjectsNWBFile class to the SubjectsTable class
+# general_spec = self.spec.get_group("general")
+# self.unmap(general_spec)
+# self.unmap(general_spec.get_group("SubjectsTable"))
+# self.map_spec("subjects_table", general_spec.get_group("SubjectsTable"))
